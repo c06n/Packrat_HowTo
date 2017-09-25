ba6916d4f7c3ddea9649d9faed5f3a6273a5be6f
David Käthner <david.kaethner@dlr.de>
2017-09-25 15:28:19 +0200

    Cleaned .gitignore

---
+++ Packrat_HowTo.R
@@ -0,0 +1,23 @@
+# Initializing Packrat.
+packrat::init()
+
+# Tidyverse
+library(dplyr)
+library(ggplot2)
+
+demo <- mtcars %>%
+  mutate(type = row.names(.)) %>%
+  select(type, everything()) %>%
+  filter(mpg > 16) %>%
+  glimpse
+
+ggplot(demo, aes(as.factor(cyl), mpg)) + geom_boxplot()
+ggplot(demo, aes(wt, mpg)) + geom_smooth()
+
+# data.table
+library(data.table)
+demo_dt <- as.data.table(demo)
+demo_dt[, cyl := cyl + 1]
+
+# Export the whole thing
+packrat::bundle(file = '~/projects/R_pkg_dev/Packrat_HowTo/export.tar.gz', overwrite = TRUE)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Packrat_HowTo
-Tutorial and explanation of reproducible package management with R/Packrat.
+
+Tutorial and explanation of reproducible package management with R and Packrat. Repository does not represent an entire Packrat repository. You can produce that by copying the ```Packrat_HowTo.R```-file and run the code.
+
